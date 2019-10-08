bc7be7d4db6ccca116a557e2fa5d657e53e7373e
Metgy Alexandre <alexandre.metgy@gmail.com>
2019-10-09 01:16:29 +0200

    Fix Maze unit tests.

--- MazeWPF.UnitTests/Maze_Tests.cs
+++ MazeWPF.UnitTests/Maze_Tests.cs
@@ -1,43 +1,15 @@
-﻿using MazeWPF.Models;
-using Xunit;
+﻿using Xunit;
 using FluentAssertions;
 using System.Linq;
-using System;
 
 namespace MazeWPF.UnitTests
 {
     public class Maze_Tests
     {
         [Fact]
-        public void GetWallBetween_NonNeighboringCells_ThrowsException()
-        {
-            var maze = new Maze(10, 10, (0, 0), (0, 3));
-            var cell1 = maze[0, 0];
-            var cell2 = maze[1, 1];
-
-            Action act = () => maze.GetWallBetween(cell1, cell2);
-
-            act.Should().Throw<ArgumentException>();
-        }
-
-        [Fact]
-        public void GetWallBetween_NeighboringCells_ReturnsTheWall()
-        {
-            var maze = new Maze(10, 10, (0, 0), (0, 3));
-            var cell1 = maze[0, 0];
-            var cell2 = maze[0, 1];
-
-            var wall = maze.GetWallBetween(cell1, cell2);
-
-            wall
-                .Should()
-                .Match<Wall>(w => (w.Cell1 == cell1 && w.Cell1 == cell2) || (w.Cell2 == cell1 && w.Cell2 == cell2));
-        }
-
-        [Fact]
         public void GetNeighbours_OfCornerCell_ReturnsTwoNeighbours()
         {
-            var maze = new Maze(10, 10, (0, 0), (0, 3));
+            var maze = new Maze(10, 10);
             var cell = maze[0, 0];
 
             var neighbours = maze.GetNeighbours(cell);
@@ -51,7 +23,7 @@ namespace MazeWPF.UnitTests
         [Fact]
         public void GetNeighbours_OfStrictBorderCell_ReturnsThreeNeighbours()
         {
-            var maze = new Maze(10, 10, (0, 0), (0, 3));
+            var maze = new Maze(10, 10);
             var cell = maze[4, 0];
 
             var neighbours = maze.GetNeighbours(cell);
@@ -65,7 +37,7 @@ namespace MazeWPF.UnitTests
         [Fact]
         public void GetNeighbours_OfInsideCell_ReturnsFourNeighbours()
         {
-            var maze = new Maze(10, 10, (0, 0), (0, 3));
+            var maze = new Maze(10, 10);
             var cell = maze[3, 3];
 
             var neighbours = maze.GetNeighbours(cell);
